1c5596054c100a0520965ac4001c0a84208ba5c6
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-12-31 18:25:01 +0500

    Added user controller routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { getNewRefreshToken, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getNewRefreshToken,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetails,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import multer from "multer";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -7,25 +19,42 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Define the POST route
-router.post('/register', (req, res, next) => {
+router.post(
+  "/register",
+  (req, res, next) => {
     upload.fields([
-        { name: "avatar", maxCount: 1 },
-        { name: "coverImage", maxCount: 1 }
+      { name: "avatar", maxCount: 1 },
+      { name: "coverImage", maxCount: 1 },
     ])(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            return res.status(400).json({ error: err.message }); // Multer-specific errors
-        } else if (err) {
-            return res.status(500).json({ error: err.message }); // General errors
-        }
-        next();
+      if (err instanceof multer.MulterError) {
+        return res.status(400).json({ error: err.message }); // Multer-specific errors
+      } else if (err) {
+        return res.status(500).json({ error: err.message }); // General errors
+      }
+      next();
     });
-}, registerUser);
+  },
+  registerUser
+);
 
-
-router.post("/login" , loginUser);
+router.post("/login", loginUser);
 
 // secure routes
-router.post('/logout' , verifyJWT , logoutUser);
-router.post("/refresh-token" , getNewRefreshToken);
+router.post("/logout", verifyJWT, logoutUser);
+router.post("/refresh-token", getNewRefreshToken);
+router.post("/change-password", verifyJWT, changeCurrentPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+router.patch("/edit-user", verifyJWT, updateUserDetails);
+
+router.patch("/avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.patch(
+  "/cover-image",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
+
+router.get("/:username", verifyJWT, getUserChannelProfile);
+router.get("/watch-history", verifyJWT, getWatchHistory);
 
 export default router;
