fd9481199f47dab0ec704dd7deeadf21bcf6b3ba
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-01-09 21:04:56 +0500

    Added Logic for subscriber controller methods.

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -40,9 +40,102 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  if (!channelId) {
+    throw new ApiError(400, "Pleae Add the required id");
+  }
+
+  const userChannelSubscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: channelId,
+      },
+    },
+    {
+      $lookup: {
+        from: "User",
+        foreignField: "_id",
+        localField: "subscriber",
+        as: "subscribers",
+      },
+    },
+    {
+      $addFields: {
+        userSubscribers: {
+          $size: "$subscribers",
+        },
+        name: "$subscribers.firstname",
+        username: "$subscriber.username",
+        avatar: "$subscribers.avatar",
+      },
+    },
+    {
+      $project: {
+        userSubscribers: 1,
+        name: 1,
+        username: 1,
+        avatar: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        userChannelSubscribers,
+        "Successfully reterived channel Subscribers"
+      )
+    );
 });
 
-const getSubscriberdChannels = asyncHandler(async (req, res) => {});
+const getSubscriberdChannels = asyncHandler(async (req, res) => {
+  const { subscriberId } = req.params;
+  if (!subscriberId) {
+    throw new ApiError(400, "Please Provide the given parameter id");
+  }
+
+  const userSubscriberedChannles = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: subscriberId,
+      },
+    },
+    {
+      $lookup: {
+        from: "User",
+        foreignField: "_id",
+        localField: "channel",
+        as: "subscribedChannels",
+      },
+    },
+    {
+      $addFields: {
+        name: "$subscribedChannels.fullname",
+        avatar: "$subscribedChannels.avatar",
+        username: "$subscribedChannels.username",
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        avatar: 1,
+        username: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        userSubscriberedChannles,
+        "Successfully reterived User Subscribed Channels"
+      )
+    );
+});
 
 export {
   toggleSubscription,
