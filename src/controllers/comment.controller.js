b6041e2a21e68720703d455bfee9768ed6d044c3
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-01-14 19:32:16 +0500

    Added Routes for the user comments manipulation

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,157 +4,135 @@ import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const addComment = asyncHandler(async (req , res) => {
-    const { content , video_id} = req.body;
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { content } = req.body;
+  console.log(content);
+  if (!(content || videoId)) {
+    throw new ApiError(400, "Video with this Id does not exists");
+  }
 
-    if(!(content || video_id)){
-        throw new ApiError(
-            400,
-            "Video with this Id does not exists"
-        )
-    }
+  const user_id = req.user?._id;
 
-    const user_id = req.user?._id;
+  if (!user_id) {
+    throw new ApiError(400, "Unauthorized User");
+  }
 
-    if(!user_id) {
-        throw new ApiError(
-            400,
-            "Unauthorized User"
-        )
-    }
+  const comment = await Comment.create({
+    content: content,
+    video: videoId,
+    owner: user_id,
+  });
 
-    const comment = await Comment.create({
-        content : content,
-        video : video_id,
-        owner : user_id
-    });
+  const createdComment = await Comment.findById(comment._id);
 
-    const createdComment = await Comment.findById(comment._id);
-
-    return res.status(201).json(
-        new ApiResponse(
-            200,
-            createdComment,
-            "Comment Added Successfully"
-        )
-    );
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdComment, "Comment Added Successfully"));
 });
 
-const updateComment = asyncHandler(async(req , res) => {
-    const comment_id = req.params;
-    const {content} = req.body;
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
 
-    if(!(content , req.params)){
-        throw new ApiError(
-            400,
-            "No matching comment found associated to this id"
-        );
+  if (!(content, req.params)) {
+    throw new ApiError(400, "No matching comment found associated to this id");
+  }
+
+  const newComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content: content,
+      },
+    },
+    {
+      new: true,
     }
+  ).select("-owner -video");
 
-    const newComment = await Comment.findByIdAndUpdate(
-        comment_id,
-        {
-            $set : {
-                content : content
-            }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newComment, "Comment Upated Successfully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!commentId) {
+    throw new ApiError(404, "Please add a reference to comment");
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(400, "Error Deleting Comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment Deleted Successfully"));
+});
+
+const getAllComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Unalble to get the comments");
+  }
+
+  const allComments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId("678236405c99d1d19dcecc68"),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "owner",
+        as: "user",
+      },
+    },
+    {
+      $unwind: "$user",
+    },
+    {
+      $group: {
+        _id: "$video",
+        userComments: {
+          $push: {
+            username: "$user.username",
+            avatar: "$user.avatar",
+            content: "$content",
+          },
         },
-        {
-            new : true
-        }
-    ).select("-owner -video");
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        userComments: 1,
+      },
+    },
+  ]);
 
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            newComment,
-            "Comment Upated Successfully"
-        )
+  if (allComments.length === 0) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "No comments associated to this video"));
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        allComments,
+        "Successfully fetched the comments assocaited to this video"
+      )
     );
 });
 
-const deleteComment = asyncHandler(async (req , res) =>{
-    const comment_id = req.params;
-
-    if(!comment_id) {
-        throw new ApiError(
-            404,
-            "Please add a reference to comment"
-        )
-    }
-
-    const deletedComment = await Comment.findByIdAndDelete(
-        comment_id
-    );
-
-    if(!deletedComment) {
-        throw new ApiError(
-            400,
-            "Error Deleting Comment"
-        )
-    }
-
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "Comment Deleted Successfully"
-        )
-    )
-})
-
-const getAllComments = asyncHandler(async (req , res) => {
-    const {videoId} = req.params;
-
-    if(!videoId) {
-        throw new ApiError(
-            400,
-            "Unalble to get the comments"
-        );
-    }
-
-    const allComments = await Comment.aggregate(
-        [
-            {
-                $match : {
-                    video : videoId
-                }
-            },
-            {
-                $lookup : {
-                    from : "Video",
-                    foreignField : "_id",
-                    localField : "video",
-                    as : "comment"
-                }
-            },
-            {
-                $lookup : {
-                    from : "User",
-                    foreignField : "_id",
-                    localField : "owner",
-                    as : "user"
-                }
-            },{
-                $addFields : {
-                    fullName : "$user.fullname",
-                    username : "$user.usename",
-                    avatar : "$user.avatar"
-                }
-            },
-            {
-                $project : {
-                    fullName : 1,
-                    username : 1,
-                    avatar : 1,
-                    content : 1
-                }
-            }
-        ]
-    )
-});
-
-export {
-    addComment,
-    updateComment,
-    deleteComment,
-    getAllComments
-}
+export { addComment, updateComment, deleteComment, getAllComments };
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { addComment, deleteComment, getAllComments, updateComment } from "../controllers/comment.controller.js";
 
 const commentRouter = new Router();
 
 // Comment Routes
 commentRouter.use(verifyJWT);
+commentRouter.post('/:videoId' , addComment);
+commentRouter.patch('/:commentId' , updateComment);
+commentRouter.delete('/:commentId' , deleteComment);
+commentRouter.get('/video-comments/:videoId' , getAllComments);
 
 export default commentRouter;
